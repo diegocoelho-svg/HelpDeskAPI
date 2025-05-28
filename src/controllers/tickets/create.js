739bcf966c5961e28cbf6da8d08b8ac4585a546a
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-05-27 21:35:07 -0300

    Criando objeto de ticket

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,3 +1,17 @@
+import { randomUUID } from "node:crypto"
+
 export function create({ request, response }) {
-  return response.end("Teste POST, Criado com sucesso!")
+  const { equipment, description, user_name } = request.body
+
+  const ticket = {
+    id: randomUUID(),
+    equipment,
+    description,
+    user_name,
+    status: "open",
+    created_at: new Date(),
+    updated_at: new Date(),
+  }
+
+  return response.writeHead(201).end(JSON.stringify(ticket))
 }
