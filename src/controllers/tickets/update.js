f6e8ad7a07546f90b4e72eb6ebdeca06ac790a06
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-05-28 21:30:54 -0300

    obtendo os dados do ticket | Atualizando o ticket

--- src/controllers/tickets/update.js
+++ src/controllers/tickets/update.js
@@ -1,3 +1,13 @@
 export function update({ request, response, database }) {
-  return response.end("ok")
+
+  const { id } = request.params
+  const { equipment, description } = request.body
+
+  database.update("tickets", id, {
+    equipment,
+    description,
+    updated_at: new Date(),
+  })
+
+  return response.writeHead(200).end()
 }
