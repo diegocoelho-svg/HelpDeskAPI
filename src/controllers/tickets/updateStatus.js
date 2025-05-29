f358af6f1d09ac407d65723cdb2879af0a41955c
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-05-28 22:01:44 -0300

    Adicionando Solução

--- src/controllers/tickets/updateStatus.js
+++ src/controllers/tickets/updateStatus.js
@@ -1,7 +1,8 @@
 export function updateStatus({ request, response, database }) {
   const { id } = request.params
+  const { solution } = request.body
 
-  database.update("tickets", id, { status: "closed" })
+  database.update("tickets", id, { status: "closed", solution })
 
   response.writeHead(200).end()
 }
